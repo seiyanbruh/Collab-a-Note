677f69b9ebcc5e002d833d9175f9eef1fa0f8426
Steven Smith <sasmith10@student.ysu.edu>
2020-10-29 12:01:48 -0400

    commit

--- src/view/components/DashboardPage/DashBoardNotesPage.js
+++ src/view/components/DashboardPage/DashBoardNotesPage.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// import React, { useEffect, useState } from "react";
-// import NoteItem from "./NoteItem";
-// import "../../styles/DashBoard.css";
-=======
 import React, { useEffect, useState } from 'react';
 import NoteItem from './NoteItem';
 import "../../styles/DashBoard.css"
@@ -11,78 +6,34 @@ import AddIcon from '@material-ui/icons/Add';
 import { getNotes, addNote } from "../../../controller/note"
 import AddNoteForm from './AddNoteForm';
 
->>>>>>> bf605b960b90950536d65cd4b546b428987f0b84
 
-// const DashBoardNotes = (props) => {
-//   const [notes, setNotes] = useState([]);
+ const DashBoardNotes = (props) => {
+ const [notes, setNotes] = useState([]);
 
-<<<<<<< HEAD
-//   const addNotesIconStyle = {
-//     margin: 0,
-//     width: "80px",
-//     height: "80px",
-//     top: "auto",
-//     right: 20,
-//     bottom: 20,
-//     left: "auto",
-//     position: "fixed",
-//   };
 
-//   useEffect(() => {
-//     setNotes(this.props.location.classItem.notes);
-//   }, []);
-=======
     const [notes, setNotes] = useState([]);
     
     const [addNoteForm, setAddNoteForm] = useState(false)
->>>>>>> bf605b960b90950536d65cd4b546b428987f0b84
 
-//   return (
-//     <div>
-//       <h1 className="dashboard-body-title">Notes</h1>
-//       <Fab
-//         color="primary"
-//         aria-label="add"
-//         style={addClassIconStyle}
-//         onClick={() => setAddClassForm(true)}
-//       >
-//         <AddIcon />
-//       </Fab>
-//       {notes.map((note) => {
-//         <NoteItem
-//           title={note.title}
-//           dateCreation={note.dateCreation}
-//           dateUpdate={note.lastUpdate}
-//         />;
-//       })}
+    return (
+     <div>
+       <h1 className="dashboard-body-title">Notes</h1>
+       <Fab
+         color="primary"
+         aria-label="add"
+         style={addClassIconStyle}
+         onClick={() => setAddClassForm(true)}
+       >
+        <AddIcon />
+       </Fab>
+       {notes.map((note) => {
+         <NoteItem
+           title={note.title}
+           dateCreation={note.dateCreation}
+           dateUpdate={note.lastUpdate}
+         />;
+       })}
 
-<<<<<<< HEAD
-//       {/* <NoteItem
-//                 title="Program Design"
-//                 dateCreation="Aug, 28th"
-//                 dateUpdate="Aug, 30th"
-//             />
-//             <NoteItem
-//                 title="Agile"
-//                 dateCreation="Aug, 28th"
-//                 dateUpdate="Aug, 30th"
-//             />
-//             <NoteItem
-//                 title="Design Pattern"
-//                 dateCreation="Aug, 28th"
-//                 dateUpdate="Aug, 30th"
-//             />
-//             <NoteItem
-//                 title="Midterm Review"
-//                 dateCreation="Aug, 28th"
-//                 dateUpdate="Aug, 30th"
-//             /> */}
-//     </div>
-//   );
-// };
-
-// export default DashBoardNotes;
-=======
     const handleAddNote = (newNote) => {
         addNote(newNote, (response) => {
             if(response.status){
@@ -93,7 +44,7 @@ import AddNoteForm from './AddNoteForm';
             setAddNoteForm(false)
         })
     }
-
+    
     useEffect(() => {
         const classItem = props.location.state 
         console.log(classItem);
@@ -127,7 +78,7 @@ import AddNoteForm from './AddNoteForm';
                 />
             ))}
 
-            {/* <NoteItem
+             <NoteItem
                 title="Program Design"
                 dateCreation="Aug, 28th"
                 dateUpdate="Aug, 30th"
@@ -146,11 +97,10 @@ import AddNoteForm from './AddNoteForm';
                 title="Midterm Review"
                 dateCreation="Aug, 28th"
                 dateUpdate="Aug, 30th"
-            /> */}
+            /> 
         </div>
     )
-}
 
 
-export default DashBoardNotes
->>>>>>> bf605b960b90950536d65cd4b546b428987f0b84
+
+export default DashBoardNotesPage
