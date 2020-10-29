bf605b960b90950536d65cd4b546b428987f0b84
Adron <dungnguyenadron@gmail.com>
2020-10-29 10:18:04 -0500

    Updates dashboard

--- src/view/components/DashboardPage/DashBoardNotesPage.js
+++ src/view/components/DashboardPage/DashBoardNotesPage.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import NoteItem from './NoteItem';
 import "../../styles/DashBoard.css"
+import { Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import { getNotes, addNote } from "../../../controller/note"
+import AddNoteForm from './AddNoteForm';
+
 
 const DashBoardNotes = (props) => {
 
     const [notes, setNotes] = useState([]);
-
+    
+    const [addNoteForm, setAddNoteForm] = useState(false)
 
     const addNotesIconStyle = {
         margin: 0,
@@ -18,24 +24,49 @@ const DashBoardNotes = (props) => {
         position: 'fixed',
     };
 
+    const handleAddNote = (newNote) => {
+        addNote(newNote, (response) => {
+            if(response.status){
+                setNotes(previousNotes => [...previousNotes, newNote])
+            } else {
+                //Handle Error
+            }
+            setAddNoteForm(false)
+        })
+    }
 
     useEffect(() => {
-        setNotes(this.props.location.classItem.notes)
+        const classItem = props.location.state 
+        console.log(classItem);
+        getNotes(classItem.id, (response) => {
+            if(response.status){
+                setNotes(response.data)
+            } else {
+                //Handle Error
+            }
+        })
     }, [])
 
     return (
         <div>
             <h1 className="dashboard-body-title">Notes</h1>
-            <Fab color="primary" aria-label="add" style={addClassIconStyle} onClick={() => setAddClassForm(true)}>
+            <Fab color="primary" aria-label="add" style={addNotesIconStyle} onClick={() => {setAddNoteForm(true)}}>
                 <AddIcon/>
             </Fab>
-            {notes.map(note => {
+            <AddNoteForm
+                formStatus={addNoteForm} 
+                classItem ={props.location.state}
+                handleClose={() => setAddNoteForm(false)} 
+                addNote={(newNote) => handleAddNote(newNote)}
+            />
+            {notes.map(note => (
                 <NoteItem
+                    key={note.id}
                     title={note.title}
-                    dateCreation={note.dateCreation}
-                    dateUpdate={note.lastUpdate}
+                    dateCreation={note.creationDate.toLocaleDateString("en-US")}
+                    dateUpdate={note.lastUpdate.toLocaleDateString("en-US")}
                 />
-            })}
+            ))}
 
             {/* <NoteItem
                 title="Program Design"
